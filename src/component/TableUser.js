e17a5abd9a4319bd991455da5a48084677c74da3
Van Dao <dao28901@gmail.com>
2025-01-21 11:53:07 +0700

    #21 Loading Status - Trạng Thái Fetch APIs

--- src/component/TableUser.js
+++ src/component/TableUser.js
@@ -2,9 +2,12 @@ import { Container, Table } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllUser } from "../action/actions";
+import "./loading.css";
 const TableUser = (props) => {
   let dispatch = useDispatch();
   const listUserRedux = useSelector((state) => state.user.listUser);
+  const isLoading = useSelector((state) => state.user.isLoading);
+  const isError = useSelector((state) => state.user.isError);
   // const [listUser, setListUser] = useState();
   useEffect(() => {
     dispatch(fetchAllUser());
@@ -14,42 +17,73 @@ const TableUser = (props) => {
   };
   return (
     <>
-      <Container className="mt-5">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>User Name</th>
-              <th>Email</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {listUserRedux &&
-              listUserRedux.length > 0 &&
-              listUserRedux.map((item, index) => {
-                return (
-                  <tr key={`item-${index}`}>
-                    <td>{item.id}</td>
-                    <td>{item.email}</td>
-                    <td>{item.username}</td>
-                    <td>
-                      <button className="btn btn-outline-primary mx-2">
-                        Change
-                      </button>
-                      <button
-                        className="btn btn-outline-danger"
-                        onClick={() => handleDeleteUser(item)}
-                      >
-                        Remove
-                      </button>
-                    </td>
+      {isError ? (
+        <div>Error.......</div>
+      ) : (
+        <>
+          {isLoading ? (
+            <div
+              aria-label="Orange and tan hamster running in a metal wheel"
+              role="img"
+              className="wheel-and-hamster"
+            >
+              <div className="wheel"></div>
+              <div className="hamster">
+                <div className="hamster__body">
+                  <div className="hamster__head">
+                    <div className="hamster__ear"></div>
+                    <div className="hamster__eye"></div>
+                    <div className="hamster__nose"></div>
+                  </div>
+                  <div className="hamster__limb hamster__limb--fr"></div>
+                  <div className="hamster__limb hamster__limb--fl"></div>
+                  <div className="hamster__limb hamster__limb--br"></div>
+                  <div className="hamster__limb hamster__limb--bl"></div>
+                  <div className="hamster__tail"></div>
+                </div>
+              </div>
+              <div className="spoke"></div>
+            </div>
+          ) : (
+            <Container className="mt-5">
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>User Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
                   </tr>
-                );
-              })}
-          </tbody>
-        </Table>
-      </Container>
+                </thead>
+                <tbody>
+                  {listUserRedux &&
+                    listUserRedux.length > 0 &&
+                    listUserRedux.map((item, index) => {
+                      return (
+                        <tr key={`item-${index}`}>
+                          <td>{item.id}</td>
+                          <td>{item.email}</td>
+                          <td>{item.username}</td>
+                          <td>
+                            <button className="btn btn-outline-primary mx-2">
+                              Change
+                            </button>
+                            <button
+                              className="btn btn-outline-danger"
+                              onClick={() => handleDeleteUser(item)}
+                            >
+                              Remove
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+            </Container>
+          )}
+        </>
+      )}
     </>
   );
 };
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -5,21 +5,26 @@ import {
 } from "../action/types";
 const initState = {
   listUser: [],
+  isLoading: false,
+  isError: false,
 };
 const userReducer = (state = initState, action) => {
   switch (action.type) {
     case FETCH_USER_START:
       return {
         ...state,
+        isLoading: true,
       };
     case FETCH_USER_SUCCESS:
       state.listUser = action.payload;
       return {
         ...state,
+        isLoading: false,
       };
     case FETCH_USER_FAILED:
       return {
         ...state,
+        isError: true,
       };
     default:
       return state;
